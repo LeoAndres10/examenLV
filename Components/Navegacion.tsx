de174b9c8f087b02f64206cdab32f2d2b562095a
LeoAndres10 <leo1318velasquez@hotmail.es>
2025-09-16 19:03:07 -0600

    Examen Finalizado

--- Components/Navegacion.tsx
+++ Components/Navegacion.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { NavigationContainer } from '@react-navigation/native';
@@ -11,16 +11,27 @@ export default function NavBar() {
   const tab=createBottomTabNavigator();
 
   return (
-    <View>
+    <View >
         <Text>Bienvenido a su banco de Preferencia</Text>
     
-    <NavigationContainer>
+    <NavigationContainer >
         <tab.Navigator>
             <tab.Screen name='Inicio' component={Inicio}></tab.Screen>
              <tab.Screen name='Transferencia' component={Transferencia}></tab.Screen>
-              <tab.Screen name='Historico' component={Historial}></tab.Screen>
+              <tab.Screen name='Historial' component={Historial}></tab.Screen>
         </tab.Navigator>
     </NavigationContainer>
     </View>
   )
+  
 }
+
+const styles = StyleSheet.create({
+  container: {
+   
+    justifyContent: 'center', 
+    alignItems: 'center',     
+    backgroundColor: '#ffefefff',
+  },
+ 
+});
